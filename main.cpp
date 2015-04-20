dfa799e83a10e32d1f544980be853fd7c1e0d927
João de Deus <joaodeusmorgado@yahoo.com>
2015-04-20 23:31:46 +0100

    update to qsrand() in main()

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    srand(time(0));
+    QTime time = QTime::currentTime();
+    qsrand((uint)time.msec());
 
     if ( !QGLFormat::hasOpenGL() )
     {
